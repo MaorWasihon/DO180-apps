3c4c330ce77e39af7b4e2d6294fa7af9400c8f3b
maor-wasihon <62095365+MaorWasihon@users.noreply.github.com>
2022-04-12 11:24:30 +0300

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def home():
    return render_template('index.html')
+
+
+@app.route("/who")
+def who():
+   return "My name is TONY MONTOYA !"
+
+
+@app.route("/health")
+def health():
+   return "{ health : ok }"
+
+
+
+
 
 
 if __name__ == "__main__":
